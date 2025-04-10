31c4f474ac8d35a86cbc1dbf4b5bfc94e5dc80d4
Karen Khoury <129860242+KarenKhou@users.noreply.github.com>
2025-04-10 12:56:58 +0300

    Delete app directory

--- app/src/main/java/com/example/tesy2/data/models/ApiResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.tesy2.data.models
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class ApiResponse(
-    val result: String // adapte en fonction de ce que ton API renvoie
-)
