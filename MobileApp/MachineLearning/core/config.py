3124f34e5b62ad23a980b48c73a0f6de206775da
Karen Khoury <129860242+KarenKhou@users.noreply.github.com>
2025-04-10 13:00:17 +0300

    Delete MobileApp/MachineLearning directory

--- MobileApp/MachineLearning/core/config.py
+++
@@ -1,6 +0,0 @@
-from supabase import create_client, Client
-
-url = "https://rnjccfpgdpzkoptzvcgr.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuamNjZnBnZHB6a29wdHp2Y2dyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MjU3OTAxMiwiZXhwIjoyMDU4MTU1MDEyfQ.-pmxBnHBuJa9Q9ugBbtj9zojuFBY17BJ7J_atcEqCWc"
-bucket = "picture-clothes"
-supabase: Client = create_client(url, key)
