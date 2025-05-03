90b895424a86deb92cc9440264da04f101679c5d
Karen Khoury <129860242+KarenKhou@users.noreply.github.com>
2025-05-03 18:27:30 +0300

    Update SupabaseClient.kt
    master branch

--- app/src/main/java/com/example/tesy2/data/supabase/SupabaseClient.kt
+++ app/src/main/java/com/example/tesy2/data/supabase/SupabaseClient.kt
@@ -7,8 +7,7 @@ import io.github.jan.supabase.storage.Storage
 
 val supabase = createSupabaseClient(
     supabaseUrl = "https://rnjccfpgdpzkoptzvcgr.supabase.co",
-    supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuamNjZnBnZHB6a29wdHp2Y2dyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MjU3OTAxMiwiZXhwIjoyMDU4MTU1MDEyfQ.-pmxBnHBuJa9Q9ugBbtj9zojuFBY17BJ7J_atcEqCWc"
-//very dangerous
+    supabaseKey = ""
 ) {
     install(Postgrest)
     install(Auth)
