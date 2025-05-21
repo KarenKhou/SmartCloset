19814e7e54e78e41f22493548da8b2ac99cc1652
Karen Khoury <129860242+KarenKhou@users.noreply.github.com>
2025-05-21 19:36:32 +0300

    Update SupabaseClient.kt
    modif for public repo

--- MobileApp/app/src/main/java/com/example/tesy2/data/supabase/SupabaseClient.kt
+++ MobileApp/app/src/main/java/com/example/tesy2/data/supabase/SupabaseClient.kt
@@ -7,13 +7,13 @@ import io.github.jan.supabase.storage.Storage
 
 val supabase = createSupabaseClient(
     supabaseUrl = "https://rnjccfpgdpzkoptzvcgr.supabase.co",
-    supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuamNjZnBnZHB6a29wdHp2Y2dyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MjU3OTAxMiwiZXhwIjoyMDU4MTU1MDEyfQ.-pmxBnHBuJa9Q9ugBbtj9zojuFBY17BJ7J_atcEqCWc"
+    supabaseKey = "key"
 
 ) {
     install(Postgrest)
     install(Auth) {
         alwaysAutoRefresh = true
-        autoLoadFromStorage = true // ✅ required to restore session after app restart
+        autoLoadFromStorage = true 
     }
     install(Storage)
 
